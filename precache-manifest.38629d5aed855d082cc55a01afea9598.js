384cf3f3ccf907444765562b30bd9ce15ad88bc5
Natalie Pina <natalie.a.pina@gmail.com>
2020-07-23 13:59:39 -0500

    Updates

---
+++ precache-manifest.38629d5aed855d082cc55a01afea9598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ecd1cf5f2b8d6e17b369b233802b1d2",
+    "url": "/stoic-quote/index.html"
+  },
+  {
+    "revision": "35b4e2a48c3b3e02f2a0",
+    "url": "/stoic-quote/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d4543bce412b7e44c51",
+    "url": "/stoic-quote/static/js/2.7557bb73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stoic-quote/static/js/2.7557bb73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b4e2a48c3b3e02f2a0",
+    "url": "/stoic-quote/static/js/main.bb6cc10a.chunk.js"
+  },
+  {
+    "revision": "34051974f307cfa4b97e",
+    "url": "/stoic-quote/static/js/runtime-main.a50df5be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stoic-quote/static/media/logo.5d5d9eef.svg"
+  }
+]);
