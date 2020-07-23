6dc5c230e80bec014c25f37725577dbeac364f13
Natalie Pina <natalie.a.pina@gmail.com>
2020-07-23 16:24:53 -0500

    initial style, accessing mock data and structruing markup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { quotes } = require('./mockDatabase.json')
+
+class App extends React.Component {
+  state = {
+    quote: {}
+  }
+
+  componentDidMount() {
+    this.setQuote();
+  }
+  
+  setQuote = () => {
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    this.setState({ quote });
+  }
+
+  render() {
+    const { quote, author } = this.state.quote;
+    return (  
+      <div className='wrapper'>
+        <section>
+          <p className='quote-body'>{quote}</p>
+          <p className='quote-author'>{`- ${author}`}</p>
+          <button className='quote-btn' onClick={() => this.setQuote()}>Click</button>
+        </section>
+        <footer>
+            <span className="copyright">Natalie Pina &copy;2020. </span>
+        </footer>
+      </div>
+    )
+  }
 }
 
 export default App;
